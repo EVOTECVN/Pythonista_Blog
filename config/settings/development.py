d30f6bfec8098d8231df9d651cfd2d7f8fb2dc88
Thanh Nguyen <valentinooo.dev@gmail.com>
2022-11-10 09:50:23 +0700

    feature: init code base

--- config/settings/development.py
+++ config/settings/development.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "utils",
+    "apps",
     "apps.home",
     "apps.posts",
     "apps.quizzes",
@@ -113,6 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
